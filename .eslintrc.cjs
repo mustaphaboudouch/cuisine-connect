0bb41bec78a352173eabc07b7d7647ec571f5919
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2024-01-11 23:52:18 +0100

    fix: turn off some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,12 @@ const config = {
     "plugin:tailwindcss/recommended",
   ],
   rules: {
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
     "tailwindcss/no-custom-classname": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
